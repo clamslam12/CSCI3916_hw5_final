6b17f38c17de3760949c6e622ad0a59900ab2cc2
nnbaokhang <nnbaokhang@gmail.com>
2020-04-03 21:29:29 -0600

    enforce https

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import Authentication from './components/authentication';
 import {HashRouter,Route} from 'react-router-dom';
 import { Provider } from 'react-redux'
 import store from './stores/store'
+import HttpsRedirect from 'react-https-redirect'
 
 function App() {
   return (
-      <div className="App">
-        <Provider store={store}>
-          <HashRouter>
-            <div>
-              <MovieHeader />
-              <Route exact path="/" render={()=><div />}/>
-              <Route path="/signin" render={()=><Authentication />}/>
-            </div>
-          </HashRouter>
-        </Provider>
-      </div>
+      <HttpsRedirect>
+          <div className="App">
+            <Provider store={store}>
+              <HashRouter>
+                <div>
+                  <MovieHeader />
+                  <Route exact path="/" render={()=><div />}/>
+                  <Route path="/signin" render={()=><Authentication />}/>
+                </div>
+              </HashRouter>
+            </Provider>
+          </div>
+      </HttpsRedirect>
   );
 }
 
